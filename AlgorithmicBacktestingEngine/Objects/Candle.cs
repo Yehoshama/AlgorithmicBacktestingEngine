2c656b784a5562f316c5442875be9152c137c4d2
budunoff <budunoff@gmail.com>
2025-08-05 20:29:20 +0300

    Add project files.

---
+++ AlgorithmicBacktestingEngine/Objects/Candle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmicBacktestingEngine.Objects
+{
+    /// <summary>
+    /// Represents a financial candle
+    /// </summary>
+    /// <param name="StartTime">start time of candle formation</param>
+    /// <param name="Open">open price</param>
+    /// <param name="Close">close price</param>
+    /// <param name="High">high price</param>
+    /// <param name="Low">low price</param>
+    /// <param name="Volume">total volume during candle formation</param>
+    public record Candle(
+        DateTime StartTime,
+        decimal Open,
+        decimal Close,
+        decimal High,
+        decimal Low,
+        decimal Volume
+        );
+}
---
+++ AlgorithmicBacktestingEngine/Objects/Tick.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmicBacktestingEngine.Objects
+{
+    /// <summary>
+    /// represents a single price tick
+    /// </summary>
+    /// <param name="Time">time of tick occurance</param>
+    /// <param name="Price">the price</param>
+    /// <param name="Volume">the amount of the trade as base asset</param>
+    public record Tick(
+        DateTime Time,
+        decimal Price,
+        decimal Volume
+        );
+}
